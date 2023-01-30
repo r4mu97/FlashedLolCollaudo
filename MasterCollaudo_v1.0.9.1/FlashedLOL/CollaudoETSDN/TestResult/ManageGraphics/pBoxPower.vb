894b481bf19f56f2e52c20323c084b0240fd565d
Riccardo Mucci <riccardo.mucci@kiwitron.it>
2023-01-30 10:34:33 +0100

    file CollaudoEtsdn is chenged into TestDevices

--- MasterCollaudo_v1.0.9.1/FlashedLOL/CollaudoETSDN/TestResult/ManageGraphics/pBoxPower.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class pBoxPower
-    Inherits TestResultView
-
-    Public Sub New(test_loading As Boolean, test_passed As Boolean, test_error As Boolean)
-        MyBase.New(test_loading, test_passed, test_error)
-    End Sub
-    Public Overrides Function ChangeGraphics()
-    End Function
-End Class
